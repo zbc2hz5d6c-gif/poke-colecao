185a7a48e18584cb8a9dd479d2debf66b214c08d
zbc2hz5d6c-gif <zbc2hz5d6c@privaterelay.appleid.com>
2026-09-04 08:15:17 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,3 +1,20 @@
-const CACHE='poke-colecao-v1';
-self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(['./','./index.html','./manifest.json']))));
-self.addEventListener('fetch',e=>{if(e.request.method==='GET'&&new URL(e.request.url).origin===location.origin)e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)))});
+const CACHE='poke-colecao-v4';
+const CORE=['./','./index.html','./manifest.json'];
+self.addEventListener('install',event=>{
+  self.skipWaiting();
+  event.waitUntil(caches.open(CACHE).then(c=>c.addAll(CORE).catch(()=>{})));
+});
+self.addEventListener('activate',event=>{
+  event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()));
+});
+self.addEventListener('fetch',event=>{
+  const u=new URL(event.request.url);
+  if(u.origin!==location.origin)return;
+  event.respondWith(
+    fetch(event.request).then(r=>{
+      const copy=r.clone();
+      caches.open(CACHE).then(c=>c.put(event.request,copy)).catch(()=>{});
+      return r;
+    }).catch(()=>caches.match(event.request).then(r=>r||caches.match('./index.html')))
+  );
+});
